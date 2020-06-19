c7e33e951b0b552446d1ed1323b66755b3ec9e03
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-06-19 16:16:39 +0300

    Add Customers api and connect the api with customer component

--- AspNetCoreWithAngular/ClientApp/src/app/customer/customer.component.ts
+++ AspNetCoreWithAngular/ClientApp/src/app/customer/customer.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-customer-component',
+  selector: 'app-customer',
   templateUrl: './customer.component.html'
 })
 export class CustomerComponent {
+  public customers: Customer[];
 
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Customer[]>(baseUrl + 'api/customers').subscribe(result => {
+      this.customers = result;
+    }, error => console.error(error));
+  }
+}
+
+class Customer {
+  id: number;
+  firstName: number;
+  lastName: string;
 }
---
+++ AspNetCoreWithAngular/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWithAngular.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public List<Customer> Get()
+        {
+            return new List<Customer>
+            {
+                new Customer{Id = 1, FirstName = "Muhammet", LastName = "Kasarcı"},
+                new Customer{Id = 2, FirstName = "Furkan", LastName = "Kasarcı"},
+                new Customer{Id = 3, FirstName = "Ömer Faruk", LastName = "Kasarcı"}
+            };
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
